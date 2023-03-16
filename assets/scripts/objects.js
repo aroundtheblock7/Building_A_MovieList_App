39a98b4e76d04e23180762aceeab43086f3d39da
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2023-03-16 06:40:39 -0400

    update

---
+++ assets/scripts/objects.js
@@ -0,0 +1,93 @@
+"use-strict";
+const searchBtn = document.getElementById('search-btn');
+const addMovieBtn = document.getElementById('add-movie-btn');
+
+const movies = [];
+
+
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+
+    if(movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+
+    movieList.innerHTML = '';
+
+    const filteredMovies = filter ? movies.filter(movie => movie.info.title.includes(filter)) : movies;
+
+    filteredMovies.forEach((movie) =>  {
+       const movieEl = document.createElement('li');
+       //movieEl.textContent = movie.info.title;
+       const { info, ...otherProps } = movie;
+       console.log(otherProps);
+       //const { title: movieTitle } = info;
+       let { getFormattedTitle } = movie;
+       //getFormattedTitle = getFormattedTitle.bind(movie);
+       //let text = movieTitle + ' - ';
+       //let text = getFormattedTitle + ' - ';
+       //let text = movie.getFormattedTitle() + ' - ';
+       let text = getFormattedTitle.call(movie) + ' - ';
+       for (const key in info) {
+        if (key !== 'title' && key !== '_title') {
+            text = text + `${key}: ${info[key]}`
+        }
+       }
+       movieEl.textContent = text;
+       movieList.append(movieEl);
+    });
+}
+
+const addMovieHandler = () => {
+    const title = document.getElementById("title").value; 
+    const extraName = document.getElementById("extra-name").value;
+    const extraValue = document.getElementById("extra-value").value;
+
+    if(
+        extraName.trim() === '' || extraValue.trim() === ''
+     ) {
+     return;
+    }
+
+    const newMovie = {
+        info: {
+            set title(val) {
+                if (val.trim() === '') {
+                    this._title = 'Default Title';
+                    return;
+                }
+                this._title = val;
+            },
+            get title() {
+               return this._title;
+            },
+            [extraName]: extraValue
+        },
+        id: Math.random(),
+        getFormattedTitle() {
+            console.log(this);
+            return this.info.title.toUpperCase();
+        }
+    };
+
+    newMovie.info.title = title;
+    console.log(newMovie.info.title);
+
+    movies.push(newMovie);
+    console.log(newMovie);
+    renderMovies();
+};
+
+const searchMovieHandler = () => {
+    console.log(this);
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+
+}
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
+
